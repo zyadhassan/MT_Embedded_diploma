cb8552ce7be0f6786a8e7b62e866b6a55db3ef43
Zyad Hassan <zyad_hassan_2020@outlook.com>
2022-08-30 14:53:52 +0200

    Add files via upload

---
+++ Week_3_C/LinkedList.c
@@ -0,0 +1,38 @@
+
+/*
+ *               LinkedList.c
+ *  The Solution of Linked List Assignment in C course by Eng.Mohamed Tarek
+ *         Zyad Hassan
+ *
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+///////////////////////////Question 1////////////////////////////////////
+
+void A6_Q1(void){
+
+
+	return;
+}
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+int main(void){
+
+
+	A6_Q1();
+return 0;
+}
+
+
